437d944c111ded1a980092211b200e6d46889405
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-03-25 16:58:48 +0100

    Modified Salmon class and change implemented interfaces

--- src/Salmon.java
+++ src/Salmon.java
@@ -1,10 +1,17 @@
-public class Salmon implements Voice, Paws {
+public class Salmon implements Animal {
+
+    private int paws = 0;
+
+    public void setPaws(int paws) {
+        this.paws = paws;
+    }
 
     @Override
-    public void giveVoice () {System.out.println("Blub blub"); }
+    public String giveVoice () {
+        return "Blub blub"; }
 
     @Override
     public Integer pawsNumber() {
-        return 0;
+        return paws;
     }
 }
