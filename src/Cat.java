da1dceebc7643dfbb113b49a6371a13f5b3ef285
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-03-25 16:58:01 +0100

    Modified Cat class and change implemented interfaces

--- src/Cat.java
+++ src/Cat.java
@@ -1,12 +1,18 @@
-public class Cat implements Voice, Paws {
+public class Cat implements Animal {
+
+    private int paws = 4;
+
+    public void setPaws(int paws) {
+        this.paws = paws;
+    }
 
     @Override
-    public void giveVoice() {
-        System.out.println("Miauuuu");
+    public String giveVoice() {
+        return"Miauuuu";
     }
 
     @Override
     public Integer pawsNumber() {
-        return 4;
+        return paws;
     }
 }
