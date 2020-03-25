3dcf3547f7139595fb77df6b3da1b2b353951583
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-03-25 16:59:34 +0100

    Updated Main class for new functionalities

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,44 @@ public class Main {
         Lion lion = new Lion();
         Salmon salmon = new Salmon();
 
+        // Voice List
+        List<Voice> listVoice = new ArrayList<>();
+        listVoice.add(dog);
+        listVoice.add(cat);
+        listVoice.add(elephant);
+        listVoice.add(lion);
+        listVoice.add(salmon);
 
-        List<Voice> list = new ArrayList<>();
-        list.add(dog);
-        list.add(cat);
-        list.add(elephant);
-        list.add(lion);
-        list.add(salmon);
-
-        for (Voice v : list) {
-            v.giveVoice();
+        for (Voice v : listVoice) {
+            System.out.println(v.getClass().getSimpleName() + " does " + v.giveVoice());
         }
 
+        // Paws List
+        List<Paws> listPaws = new ArrayList<>();
+        listPaws.add(dog);
+        listPaws.add(cat);
+        listPaws.add(elephant);
+        listPaws.add(lion);
+        listPaws.add(salmon);
+
+        for (Paws v : listPaws) {
+            System.out.println(v.getClass().getSimpleName() + " has " + v.pawsNumber() + " paws.");
+        }
+
+        // Animal List
+        Elephant elephant1 = new Elephant();
+        elephant1.setPaws(32);
+
+        List<Animal> listAnimal = new ArrayList<>();
+        listAnimal.add(dog);
+        listAnimal.add(cat);
+        listAnimal.add(elephant);
+        listAnimal.add(lion);
+        listAnimal.add(salmon);
+        listAnimal.add(elephant1);
+
+        for (Animal v : listAnimal) {
+            System.out.println(v.getClass().getSimpleName() + " does " + v.giveVoice() + " and has " + v.pawsNumber() + " paws.");
+        }
     }
 }
